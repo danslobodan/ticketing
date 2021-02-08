e592d820c81cacd94edf7969afc93f98d14240e7
Slobodan Dan <danastroman@gmail.com>
2021-02-08 20:39:42 +0100

    Put CommentList and CommentCreate inside of card-body div.

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -27,9 +27,9 @@ const PostList = () => {
                 >
                     <div className="card-body">
                         <h3>{post.title}</h3>
+                        <CommentList postId={post.id} />
+                        <CommentCreate postId={post.id} />
                     </div>
-                    <CommentList postId={post.id} />
-                    <CommentCreate postId={post.id} />
                 </div>
             )
         })
