7adb274d5dacc3f6c3c01441e0e6b8f3766ec9af
Slobodan Dan <danastroman@gmail.com>
2021-02-08 21:06:24 +0100

    Emmiting PostCreated event.

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +20,13 @@ app.post('/posts', (req, res) => {
     const { title } = req.body;
 
     posts[id] = { id, title };
+
+    axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
 
     res.status(201).send(posts[id]);
 });
