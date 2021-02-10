697ec98abfe2144f77adac71cc05bcadaf9a3073
Slobodan Dan <danastroman@gmail.com>
2021-02-10 22:17:38 +0100

    Added cold event handling in query service.

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +16,12 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
     const { type, data } = req.body;
 
+    handleEvent(type, data);
+
+    res.send({});
+})
+
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -39,10 +46,16 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
 
-    res.send({});
-})
+app.listen(4002, async () => {
+    console.log('Listening on 4002');
 
-app.listen(4002, () => {
-    console.log('Listening on 4002');    
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 })
