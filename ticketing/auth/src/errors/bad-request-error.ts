2f4d5ba70422f955deebc9b66398827b31a3bc6e
Slobodan Dan <danastroman@gmail.com>
2021-03-01 22:29:13 +0100

    Throwing BadRequestError from signup.

--- ticketing/auth/src/errors/bad-request-error.ts
+++ ticketing/auth/src/errors/bad-request-error.ts
@@ -1,6 +1,6 @@
 import { CustomError } from './custom-error';
 
-class BadRequestError extends CustomError {
+export class BadRequestError extends CustomError {
     statusCode: number = 400;
 
     constructor(public message: string) {
