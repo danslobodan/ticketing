735ed6a94709892c9b1c793a3d0d607b09704416
Slobodan Dan <danastroman@gmail.com>
2021-03-07 20:34:10 +0100

    Added code for fetching current user.

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,25 @@
-export default () => {
-    return <h1>Landing Page</h1>
-}
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
+
+    return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+    if (process.browser) {
+        const response = await axios.get('/api/users/currentuser');
+        return response.data;
+    }
+
+    const namespace = 'ingress-nginx';
+    const service = 'ingress-nginx-controller';
+    const domain = `${service}.${namespace}.svc.cluster.local`;
+
+    const { data } = await axios.get(`http://${domain}/api/users/currentuser`, {
+        headers: req.headers,
+    });
+    return data;
+};
+
+export default LandingPage;
