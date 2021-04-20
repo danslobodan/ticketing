2db1788b1854c51d042c6dfa5361142bd483008a
Slobodan Dan <danastroman@gmail.com>
2021-04-20 21:19:52 +0200

    Added nats-wrapper mock to all tests.

--- ticketing/tickets/src/routes/__test__/updateTicket.test.ts
+++ ticketing/tickets/src/routes/__test__/updateTicket.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 
+jest.mock('../../nats-wrapper');
+
 describe('PUT to /api/tickets/:id', () => {
     it('returns a 404 if the provided id does not exist', async () => {
         const id = new mongoose.Types.ObjectId().toHexString();
