ee26726006cc16bad473778028f3563385c9959c
Slobodan Dan <danastroman@gmail.com>
2023-05-08 19:58:08 +0200

    Implemented expiration service.

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { ExpirationCompleteEvent } from '@sdtickets/common';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import { Order, OrderStatus } from '../../../models/order';
+
+const setup = async () => {
+    const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+    const ticket = Ticket.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'concert',
+        price: 20,
+    });
+
+    await ticket.save();
+
+    const order = Order.build({
+        status: OrderStatus.Created,
+        userId: 'alskdfj',
+        expiresAt: new Date(),
+        ticket,
+    });
+
+    await order.save();
+
+    const data: ExpirationCompleteEvent['data'] = {
+        orderId: order.id,
+    };
+
+    // @ts-ignore
+    const message: Message = {
+        ack: jest.fn(),
+    };
+
+    return { listener, order, ticket, data, message };
+};
+
+it('updates the order status to cancelled', async () => {
+    const { listener, order, data, message } = await setup();
+
+    await listener.onMessage(data, message);
+
+    const updatedOrder = await Order.findById(order.id);
+
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emit an OrderCancelled event', async () => {
+    const { listener, order, data, message } = await setup();
+
+    await listener.onMessage(data, message);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+    const eventData = JSON.parse(
+        (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+    );
+
+    expect(eventData.id).toEqual(order.id);
+});
+
+it('ack the message', async () => {
+    const { listener, data, message } = await setup();
+
+    await listener.onMessage(data, message);
+
+    expect(message.ack).toHaveBeenCalled();
+});
